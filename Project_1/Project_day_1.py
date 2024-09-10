1b042986204f2b191f2d9fa92369de73b17c822f
Raul C Siqueira <95100546+RaulCSiqueira@users.noreply.github.com>
2024-09-10 17:54:50 -0300

    Add files via upload

---
+++ Project_1/Project_day_1.py
@@ -0,0 +1,4 @@
+print("Bem Vindo ao gerador de Nome de Bandas! \n")
+city = input("Que cidade você cresceu? ")
+animal = input("Qual nome do Seu animal de estimação? ")
+print(f"O nome da sua banda é {city} {animal}")
