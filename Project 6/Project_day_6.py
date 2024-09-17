b202e0ba2bd4d485fc348513e8af6fcc583b1db6
Raul C Siqueira <95100546+RaulCSiqueira@users.noreply.github.com>
2024-09-17 16:55:25 -0300

    Create Project_day_6.py

---
+++ Project 6/Project_day_6.py
@@ -0,0 +1,23 @@
+# Este programa é uma soluçao para o programa no Reborg's Wolrd
+# Site dedicado para aprender programação https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+#
+# This program is a solution for de progrma in Reborgs' World
+# Web page for learning Python https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
